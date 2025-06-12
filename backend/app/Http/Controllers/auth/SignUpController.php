2e41f9c3f267365b52c2bb69bfc764f6981b1312
Davi Dias <davifernandosousadias18@gmail.com>
2025-06-12 19:10:58 -0300

    chore: modifying default setup files and adding common routes

---
+++ backend/app/Http/Controllers/auth/SignUpController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Http\Controllers\Controller;
+use Laravel\Lumen\Http\Request;
+
+class SignUpController extends Controller
+{
+    public function SignUp(Request $data)
+    {
+        return $data;
+    }
+}
---
+++ backend/routes/auth/sign-in-route.php
@@ -0,0 +1,2 @@
+<?php
+/** @var \Laravel\Lumen\Routing\Router $router */
