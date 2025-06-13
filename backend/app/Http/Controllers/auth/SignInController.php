489991aca390919db1568ef8f0261a098c0cf3fd
Davi Dias <davifernandosousadias18@gmail.com>
2025-06-12 21:57:45 -0300

    feat: added first routes

--- backend/app/Http/Controllers/auth/SignInController.php
+++ backend/app/Http/Controllers/auth/SignInController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Laravel\Lumen\Http\Request;
 
 class SignInController extends Controller
 {
-    //
+    public function SignIn(Request $data)
+    {
+        return phpinfo();
+    }
 }
--- backend/routes/auth/sign-in-route.php
+++ backend/routes/auth/sign-in-route.php
@@ -1,2 +1,3 @@
 <?php
 /** @var \Laravel\Lumen\Routing\Router $router */
+$router->post('/user/login', 'auth\SignInController@SignIn');
